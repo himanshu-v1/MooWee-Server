dcfe98454af5811c38ceb7f4a452c4ea5e0e58b0
Himanshu Sharma <himanshu.st6@gmail.com>
2025-06-08 17:51:07 +0530

    Base setup + Working End flow for movies table

---
+++ module/dataActions.js
@@ -0,0 +1,35 @@
+import getConnection from "../utilities/connection.js";
+import mine from "../utilities/mine.js";
+import axios from 'axios';
+import config from '../config.json' with { type: "json" };
+
+const getPage = async (src, id) => {
+  const response = await axios.get(src+id);
+  const data = await response.data;
+  return data;
+};
+
+const buildData = async (data, id) => {
+  const {client, collection} = await getConnection(config.db.dataCollectionName);
+  const wall = mine(data);
+  wall.id = id;
+  try {
+    await client.connect();
+    const msg =  await collection.insertOne(wall);
+    return { msg: "Success", id: msg.insertedId };
+  } finally {
+    client.close();
+  }
+};
+
+const testData = (data, id) => {
+  try {
+    const wall = mine(data);
+    wall.id = id;
+    return wall;
+  } finally {
+    console.log("Test Complete");
+  }
+};
+
+export { getPage, buildData, testData };
---
+++ router/handler.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import mine from '../utilities/mine.js';
+import config from '../config.json' with { type: "json" };
+import { getPage, buildData, testData } from '../module/dataActions.js';
+// const require = createRequire(import.meta.url);
+// const config = require('../config.json');
+
+const routes = express.Router();
+
+routes.get('/', (req, res) => {
+    return res.send("Hello World!");
+});
+
+routes.get('/:id', async (req, res) => {
+    // console.log(req.params.id);
+    const data = await getPage(config.src, req.params.id);
+    const result = mine(data, ['img']);
+    return res.send(result);
+});
+
+routes.get('/build/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = await getPage(config.src, id);
+    const msg = await buildData(data, id).catch(err => {
+        res.status(500).send({ msg: 'error', err });
+    });
+    res.send(msg);
+});
+
+routes.get('/test/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = await getPage(config.src, id);
+    const msg = testData(data, id);
+    console.log(msg);
+    res.send(msg);
+});
+
+export default routes;
